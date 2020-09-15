6f8a3593aa146d15249b482ba56d588c4497cdaf
qurek9038 <qurek9038@wrdsb.ca>
2020-09-15 00:30:29 +0000

    Finished The Hello Name Question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Greets the user and calculates their approximate age
+ * @author Khazina
  */
 public class Main {
 
@@ -9,7 +10,39 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create a scanner for a user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask the user for their name
+    System.out.println("Please enter your name");
+
+    //Declare a variable to store the name of the user
+    String name;
+
+    //Get input for the users name
+    name = input.nextLine();
+
+    //Greet the user 
+    System.out.println("Hello " + name + "!");
+
+    //Declare and initialize the current year constant
+    final int CURRENT_YEAR = 2020;
+
+    //Prompt the user to enter their birth year
+    System.out.println("Please enter the year you were born");
+
+    //Get the users birth year
+    int birthYear = input.nextInt();
+
+    //Calculate the approximate age
+    int age = CURRENT_YEAR - birthYear;
+
+    //Tell the user their approximate age
+    System.out.println("You are " + age + " years old.");
+
+
+
+
     
   }
 }
